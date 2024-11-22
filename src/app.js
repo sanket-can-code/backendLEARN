e915917692d25743e9eca1f9408567ba6a48c0f0
lendrik-kumar <sanketkamboj2006@gmail.com>
2024-11-22 17:14:23 +0530

    day2

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
-
 const app = express()
 
 app.use(cors({
@@ -15,4 +14,10 @@ app.use (express.urlencoded({extended: true , limit: "16kb"}))
 app.use (express.static("public"))
 app.use (cookieParser())
 
+
+import userRouter from './routes/user.routes.js'
+
+app.use("/api/v1/users", userRouter)
+
+ 
 export { app }
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from 'cloudinary'
+import exp from 'constants'
+import fs from 'fs'
+
+
+cloudinary.config({ 
+    cloud_name: process.env.cloud_name, 
+    api_key: process.env.api_key, 
+    api_secret: process.env.api_secret 
+})
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath){
+            return null
+        }
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        console.log(response.url)
+        return response
+    }
+    catch (error) {
+        fs.unlinkSync(localFilePath)
+    }
+}
+
+export { uploadOnCloudinary }
