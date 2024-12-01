a145d0c572f3a1b130d8f46aa315535b4ed4a735
lendrik-kumar <sanketkamboj2006@gmail.com>
2024-12-01 14:08:31 +0530

    "Updated typescript version and subscription model"

---
+++ src/models/subscription.models.js
@@ -0,0 +1,16 @@
+import { mongoose, Schema } from "mongoose"
+import { User } from "./user.models"
+
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    channel: {
+        type: Schema.Types.ObjectId,
+        ref: "User"  
+    }
+},{timestamps: true})
+
+export const subscription = mongoose.model("subscription", subscriptionSchema)
